35dc9e064717d3cae8c84f462d76f64e535aaf5c
Abu Shahriar Ratul <ratulcse10@gmail.com>
2015-05-01 00:17:51 +0600

    Footer Date Fix

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="page-footer">
     <div class="page-footer-inner">
-        2014 &copy; Studio Management.
+        {{date('Y')}} &copy; Studio Management.
     </div>
     <div class="scroll-to-top">
         <i class="icon-arrow-up"></i>
